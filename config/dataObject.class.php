9247fbbd6f1a471f7fd47d3ebd855d1c826cde77
mrmupton <mrmupton@gmail.com>
2013-03-18 14:26:31 -0400

    Initial Push

---
+++ config/dataObject.class.php
@@ -0,0 +1,47 @@
+<?php
+class dataObject {
+	private static $link = null ;
+    private static function getLink ( ) {
+        if ( self :: $link ) { return self::$link; }
+        $ini = '../_private/config.ini' ;
+        $parse = parse_ini_file ( $ini , true ) ;
+
+        $driver = $parse [ "db_driver" ] ;
+        $dsn = "${driver}:" ;
+        $user = $parse [ "db_user" ] ;
+        $password = $parse [ "db_password" ] ;
+        $options = $parse [ "db_options" ] ;
+        $attributes = $parse [ "db_attributes" ] ;
+
+        foreach ( $parse [ "dsn" ] as $k => $v ) {
+            $dsn .= "${k}=${v};" ;
+        }
+
+        try {
+			self::$link = new PDO($dsn,$user,$password,$options);
+		}
+		catch(PDOException $e){
+			die("Could not connect to the database\n");
+		}
+        foreach ( $attributes as $k => $v ) {
+            self :: $link -> setAttribute ( constant ( "PDO::{$k}" )
+                , constant ( "PDO::{$v}" ) ) ;
+        }
+
+        return self::$link ;
+    }
+
+    public static function __callStatic ( $name, $args ) {
+        $callback = array ( self :: getLink ( ), $name ) ;
+        return call_user_func_array ( $callback , $args ) ;
+    }
+}
+class dataObjectResult {
+	public $result;
+	public function __construct($dbq){
+		$db = dataObject::prepare($dbq);
+		$db->execute();
+		$this->result = $db;
+	}
+}
+?>
---
+++ config/errorReporting.class.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+ini_set( 'display_errors', 0);
+// Error Handler
+
+function handleError($errno, $errstr, $errfile, $errline)
+{
+    if (!(error_reporting() & $errno)) { return; }
+    switch ($errno) {
+		case E_USER_ERROR:
+			$errortyp =  'User Error';
+			exit(1);
+			break;	
+		case E_USER_WARNING:
+			$errortyp = 'User Warning';
+			break;	
+		case E_USER_NOTICE:
+			$errortyp = 'User Notice - This may not be an actual error';
+			break;	
+		default:
+			$errortyp = 'Unknown';
+			break;
+	}
+	$msg = '<table class="error">'."\n\t";
+	if(strlen($errstr) >= 1) { $msg .= '<tr>'."\n\t\t".'<td colspan="2">Error Message: '."\n\t\t".$errstr.'</td>'."\n\t".'</tr>'."\n\t"; }
+	if(strlen($errfile) >= 1) { $msg .= '<tr>'."\n\t\t".'<td>Found on File:</td>'."\n\t\t".'<td>'.$errfile.'</td>'."\n\t".'</tr>'."\n\t"; }
+	if(strlen($errline) >= 1) { $msg .= '<tr>'."\n\t\t".'<td>On Line:</td>'."\n\t\t".'<td>'.$errline.'</td>'."\n\t".'</tr>'."\n"; }
+	$msg .= '</table>'."\n";
+	if(strlen($msg) <=  47) { $msg .= 'Error Information not found'; }
+	echo $msg;
+    return true;
+}
+// set to the user defined error handler
+$old_error_handler = set_error_handler("handleError");
+?>
